46614518ceed5d1f48a5456ba9bcb4c68fef1fc9
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-24 12:28:21 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "gcp_project" {}
+variable "credentials" {}
+variable "zookeepervpc" {}
+variable "zookeepersub" {}
+variable "instance_name" {}
+variable "zookeeper_instance_ip_name" {}
+variable "zookeeper_instance_ip_region" {}
